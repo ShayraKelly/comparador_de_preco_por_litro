73da7d4a32c55cf86f4e942de71a8ccac5a87057
Shayra Kelly <108482817+ShayraKelly@users.noreply.github.com>
2025-10-03 21:47:51 -0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\shayr\\OneDrive\\Área de Trabalho\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\shayr\\OneDrive\\Documents\\1 pasta faculdade\\TSI\\6 período\\DDM\\Prova 1\\comparador_preco_litro" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\shayr\\OneDrive\\Área de Trabalho\\flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\shayr\\OneDrive\\Documents\\1 pasta faculdade\\TSI\\6 período\\DDM\\Prova 1\\comparador_preco_litro"
+  "FLUTTER_ROOT=C:\\Users\\shayr\\OneDrive\\Área de Trabalho\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\shayr\\OneDrive\\Documents\\1 pasta faculdade\\TSI\\6 período\\DDM\\Prova 1\\comparador_preco_litro\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\shayr\\OneDrive\\Documents\\1 pasta faculdade\\TSI\\6 período\\DDM\\Prova 1\\comparador_preco_litro"
+  "FLUTTER_TARGET=C:\\Users\\shayr\\OneDrive\\Documents\\1 pasta faculdade\\TSI\\6 período\\DDM\\Prova 1\\comparador_preco_litro\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzUuNQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049YWM0ZTc5OWQyMw==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ZDNkNDVkY2YyNQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjI="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\shayr\\OneDrive\\Documents\\1 pasta faculdade\\TSI\\6 período\\DDM\\Prova 1\\comparador_preco_litro\\.dart_tool\\package_config.json"
+)
