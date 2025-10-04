0fe8df94daf008fed5f6bcbacd4877d0f5882e28
Shayra Kelly <108482817+ShayraKelly@users.noreply.github.com>
2025-10-03 21:46:54 -0300

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/comparador_preco_litro/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.comparador_preco_litro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\shayr\OneDrive\Área de Trabalho\flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\shayr\OneDrive\Documents\1 pasta faculdade\TSI\6 período\DDM\Prova 1\comparador_preco_litro"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
